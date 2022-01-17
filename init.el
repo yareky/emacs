1a4ac9fd165a22c5e7bff46cc3525fea33ad9ee0
Jarosław Jaros <jro@spyro-soft.com>
2022-01-17 13:29:13 +0100

    Tweaks

--- init.el
+++ init.el
@@ -1,9 +1,37 @@
+;; Workaround for https://debbugs.gnu.org/34341 in GNU Emacs <= 26.3.
+(when (and (version< emacs-version "26.3") (>= libgnutls-version 30603))
+  (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3"))
+
+
+;; Write customizations to a separate file instead of this file.
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file t)
+
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
-(setq package-selected-packages '(lsp-mode yasnippet lsp-treemacs helm-lsp
-    projectile hydra flycheck company avy which-key helm-xref dap-mode))
+(setq package-selected-packages '(
+                  lsp-mode
+				  yasnippet
+				  lsp-treemacs
+				  helm-lsp
+			      projectile
+				  hydra
+				  flycheck
+				  company
+				  avy
+				  which-key
+				  helm-xref
+				  dap-mode
+				  neotree
+				  all-the-icons
+				  markdown-mode
+				  paredit
+
+				  rainbow-delimiters
+                  magit
+				  ))
 
 (when (cl-find-if-not #'package-installed-p package-selected-packages)
   (package-refresh-contents)
@@ -13,29 +41,63 @@
 (setq scroll-step            1
       scroll-conservatively  10000)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(custom-enabled-themes '(tango-dark))
- '(package-selected-packages
-   '(lsp-treemacs yasnippet lsp-ui lsp-mode flycheck-kotlin flycheck magit kotlin-mode janet-mode all-the-icons neotree)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;show column on the bottom bar
+(column-number-mode)
 
+
+(load-theme 'tango-dark)
+(set-face-background 'default "#111")
+(setq-default show-trailing-whitespace t)
+(setq-default indicate-empty-lines t)
+(setq-default indicate-buffer-boundaries 'left)
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+(setq c-basic-offset 4)
+(setq js-indent-level 2)
+(setq css-indent-offset 2)
+(setq show-paren-delay 0)
+(show-paren-mode)
+
+
+ ;'(ansi-color-faces-vector
+ ;  [default default default italic underline success warning error])
+ ;'(custom-enabled-themes '(tango-dark))
+ ;'(package-selected-packages
+ ;  '(lsp-treemacs yasnippet lsp-ui lsp-mode flycheck-kotlin flycheck magit kotlin-mode janet-mode all-the-icons neotree)))
+;(custom-set-faces
+
+(global-set-key (kbd "<backtab>") 'ace-window)
+
+; NeoTree config
 (global-set-key [f8] 'neotree-toggle)
 
 (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
 (setq neo-smart-open t)
 
+;; Enable Paredit.
+(add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
+(add-hook 'eval-expression-minibuffer-setup-hook 'enable-paredit-mode)
+(add-hook 'ielm-mode-hook 'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook 'enable-paredit-mode)
+(add-hook 'lisp-mode-hook 'enable-paredit-mode)
 
+;; Enable Rainbow Delimiters.
+(add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
+(add-hook 'ielm-mode-hook 'rainbow-delimiters-mode)
+(add-hook 'lisp-interaction-mode-hook 'rainbow-delimiters-mode)
+(add-hook 'lisp-mode-hook 'rainbow-delimiters-mode)
+
+;; Customize Rainbow Delimiters.
+(require 'rainbow-delimiters)
+(set-face-foreground 'rainbow-delimiters-depth-1-face "#c66")  ; red
+(set-face-foreground 'rainbow-delimiters-depth-2-face "#6c6")  ; green
+(set-face-foreground 'rainbow-delimiters-depth-3-face "#69f")  ; blue
+(set-face-foreground 'rainbow-delimiters-depth-4-face "#cc6")  ; yellow
+(set-face-foreground 'rainbow-delimiters-depth-5-face "#6cc")  ; cyan
+(set-face-foreground 'rainbow-delimiters-depth-6-face "#c6c")  ; magenta
+(set-face-foreground 'rainbow-delimiters-depth-7-face "#ccc")  ; light gray
+(set-face-foreground 'rainbow-delimiters-depth-8-face "#999")  ; medium gray
+(set-face-foreground 'rainbow-delimiters-depth-9-face "#666")  ; dark gray
 
 ;; sample `helm' configuration use https://github.com/emacs-helm/helm/ for details
 (helm-mode)
